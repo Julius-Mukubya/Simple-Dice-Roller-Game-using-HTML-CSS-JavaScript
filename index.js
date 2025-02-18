f59f3b0b876b93d6cfe854d045f3cf0f5fdb0ab1
Julius <mukubyajulius@gmail.com>
2025-02-18 08:02:24 +0300

    Added some comments

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-document.getElementById("rollDice").onclick = function(){
+document.getElementById("rollDice").onclick = function() {
     let diceResult;
-    let diceResults = [];
+    let diceResults = []; // Store results of all rolled dice
 
     let numberOfDices = document.getElementById("diceNum").value;
     let diceOutcomes = document.getElementById("diceOutcomes");
-    diceOutcomes.textContent = "dice: ";
+    diceOutcomes.textContent = "dice: "; // Reset text output
 
     let diceImages = document.getElementById("diceImages");
-    diceImages.innerHTML = "";
-    
-    for (let i = 0; i < numberOfDices; i++){
-        diceResult = Math.round((Math.random() * 5) + 1);
+    diceImages.innerHTML = ""; // Clear previous dice images
+
+    for (let i = 0; i < numberOfDices; i++) {
+        diceResult = Math.round((Math.random() * 5) + 1); // Generate a random dice roll (1-6)
         diceResults.push(diceResult);
 
-        let newImage = document.createElement("img");
-        switch(diceResult){
+        let newImage = document.createElement("img"); // Create a new image element
+
+        // Assign the correct image based on the dice roll
+        switch(diceResult) {
             case 1:
                 newImage.src = "images/dice-six-faces-one.png";
                 newImage.alt = "dice-face-one";
@@ -39,12 +41,14 @@ document.getElementById("rollDice").onclick = function(){
                 newImage.src = "images/dice-six-faces-six.png";
                 newImage.alt = "dice-face-six";
                 break;
-            
         }
-        diceImages.appendChild(newImage);
+
+        diceImages.appendChild(newImage); // Add the dice image to the page
     }
 
+    // Update text output with dice results
     diceOutcomes.textContent = diceOutcomes.textContent + diceResults.join(", ");
     diceOutcomes.style = "display: block;";
+
     console.log(diceResults);
-}
+};
